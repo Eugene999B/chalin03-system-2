7fb5921ec2c87f22338b76f79b372703ca6c4081
Eugene999B <kuamieugene321@gmail.com>
2026-07-21 20:22:41 -0700

    Mount Command Gate resume and emergency services

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import EmergencyCommandOverlay from "./components/EmergencyCommandOverlay.jsx";
+import { installCommandGateHistoryTracker } from "./utils/commandGateHistoryTracker.js";
 import "./index.css";
 import "./styles/userPermissionManager.mobile.css";
+
+installCommandGateHistoryTracker();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <EmergencyCommandOverlay />
   </React.StrictMode>
 );
 
