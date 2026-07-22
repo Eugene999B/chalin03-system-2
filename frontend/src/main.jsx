89412b3d99a23bee246c77f0a21705bd31b90a29
Eugene999B <kuamieugene321@gmail.com>
2026-07-21 23:34:51 -0700

    Show the Command Gate workflow after login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import EmergencyCommandOverlay from "./components/EmergencyCommandOverlay.jsx";
+import CommandArrivalBanner from "./components/CommandArrivalBanner.jsx";
 import { installCommandGateHistoryTracker } from "./utils/commandGateHistoryTracker.js";
 import "./index.css";
 import "./styles/userPermissionManager.mobile.css";
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <EmergencyCommandOverlay />
+    <CommandArrivalBanner />
   </React.StrictMode>
 );
 
