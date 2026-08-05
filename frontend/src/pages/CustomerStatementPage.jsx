30ee96c4aa50c4a5fbd7ff9e8ac56e8162354aa8
Eugene999B <kuamieugene321@gmail.com>
2026-08-05 10:04:49 +0000

    feat(customers): mount identity centre in statements

--- frontend/src/pages/CustomerStatementPage.jsx
+++ frontend/src/pages/CustomerStatementPage.jsx
@@ -1,1 +1,17 @@
-export { default } from "./CustomerStatementWorkspacePage";
+import { useState } from "react";
+
+import CustomerIdentityManagementPanel from "../components/CustomerIdentityManagementPanel";
+import CustomerStatementWorkspacePage from "./CustomerStatementWorkspacePage";
+
+export default function CustomerStatementPage() {
+  const [statementRefreshKey, setStatementRefreshKey] = useState(0);
+
+  return (
+    <>
+      <CustomerIdentityManagementPanel
+        onMerged={() => setStatementRefreshKey((current) => current + 1)}
+      />
+      <CustomerStatementWorkspacePage key={statementRefreshKey} />
+    </>
+  );
+}
