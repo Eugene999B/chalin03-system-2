c5ca5e157792c5865b740a60d9c0d2b38994ae59
Eugene999B <kuamieugene321@gmail.com>
2026-08-06 11:29:00 +0000

    feat(chalin-one): expose Content Studio frontend foundation

---
+++ frontend/src/chalin-one/content-studio/index.js
@@ -0,0 +1,4 @@
+export { default as ContentStudioWorkspace } from "./ContentStudioWorkspace";
+export { default as ContentStudioDashboard } from "./ContentStudioDashboard";
+export * from "./contentStudioApi";
+export * from "./contentStudioModel";
