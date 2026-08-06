06b94309be5edd5b14df08fecdb69191289a42f8
Eugene999B <kuamieugene321@gmail.com>
2026-08-06 12:00:21 +0000

    feat(chalin-one): export governed Newsroom manager

--- frontend/src/chalin-one/content-studio/index.js
+++ frontend/src/chalin-one/content-studio/index.js
@@ -1,6 +1,8 @@
 export { default as ContentStudioWorkspace } from "./ContentStudioWorkspace";
 export { default as ContentStudioDashboard } from "./ContentStudioDashboard";
 export { default as ContentStudioPageManager } from "./ContentStudioPageManager";
+export { default as ContentStudioNewsroomManager } from "./ContentStudioNewsroomManager";
 export * from "./contentStudioApi";
 export * from "./contentStudioPageApi";
+export * from "./contentStudioNewsroomApi";
 export * from "./contentStudioModel";
