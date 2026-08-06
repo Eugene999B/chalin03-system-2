e25717ea5b59fb1d0c5ee96ff76efb4379aa92b4
Eugene999B <kuamieugene321@gmail.com>
2026-08-06 12:46:42 +0000

    feat(chalin-one): export expanded Content Studio managers

--- frontend/src/chalin-one/content-studio/index.js
+++ frontend/src/chalin-one/content-studio/index.js
@@ -3,8 +3,15 @@ export { default as ContentStudioDashboard } from "./ContentStudioDashboard";
 export { default as ContentStudioPageManager } from "./ContentStudioPageManager";
 export { default as ContentStudioNewsroomManager } from "./ContentStudioNewsroomManager";
 export { default as ContentStudioLeadershipManager } from "./ContentStudioLeadershipManager";
+export { default as ContentStudioGovernedManager } from "./ContentStudioGovernedManager";
+export { default as ContentStudioCompanyInfoManager } from "./ContentStudioCompanyInfoManager";
+export {
+  ContentStudioProjectManager,
+  ContentStudioEquipmentManager,
+} from "./ContentStudioPortfolioManagers";
 export * from "./contentStudioApi";
 export * from "./contentStudioPageApi";
 export * from "./contentStudioNewsroomApi";
 export * from "./contentStudioPortfolioApi";
+export * from "./contentStudioCompanyInfoApi";
 export * from "./contentStudioModel";
