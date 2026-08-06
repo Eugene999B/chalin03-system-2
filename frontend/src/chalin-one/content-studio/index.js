d6242848d814cb6419b52d6b0ab2c514bcf97be5
Eugene999B <kuamieugene321@gmail.com>
2026-08-06 13:07:12 +0000

    feat(chalin-one): export complete Content Studio workspace

--- frontend/src/chalin-one/content-studio/index.js
+++ frontend/src/chalin-one/content-studio/index.js
@@ -5,13 +5,22 @@ export { default as ContentStudioNewsroomManager } from "./ContentStudioNewsroom
 export { default as ContentStudioLeadershipManager } from "./ContentStudioLeadershipManager";
 export { default as ContentStudioGovernedManager } from "./ContentStudioGovernedManager";
 export { default as ContentStudioCompanyInfoManager } from "./ContentStudioCompanyInfoManager";
+export { default as ContentStudioMediaManager } from "./ContentStudioMediaManager";
+export { default as ContentStudioFormManager } from "./ContentStudioFormManager";
 export {
   ContentStudioProjectManager,
   ContentStudioEquipmentManager,
 } from "./ContentStudioPortfolioManagers";
+export {
+  ContentStudioApprovalInbox,
+  ContentStudioEnquiryDesk,
+  ContentStudioNavigationManager,
+  ContentStudioSettingsManager,
+} from "./ContentStudioOperationalManagers";
 export * from "./contentStudioApi";
 export * from "./contentStudioPageApi";
 export * from "./contentStudioNewsroomApi";
 export * from "./contentStudioPortfolioApi";
 export * from "./contentStudioCompanyInfoApi";
+export * from "./contentStudioOperationsApi";
 export * from "./contentStudioModel";
