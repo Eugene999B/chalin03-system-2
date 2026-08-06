2f1bd9f24d4b67013938ea3d9edd9334209c075a
Eugene999B <kuamieugene321@gmail.com>
2026-08-06 11:39:52 +0000

    feat(chalin-one): export governed Page Manager

--- frontend/src/chalin-one/content-studio/index.js
+++ frontend/src/chalin-one/content-studio/index.js
@@ -1,4 +1,6 @@
 export { default as ContentStudioWorkspace } from "./ContentStudioWorkspace";
 export { default as ContentStudioDashboard } from "./ContentStudioDashboard";
+export { default as ContentStudioPageManager } from "./ContentStudioPageManager";
 export * from "./contentStudioApi";
+export * from "./contentStudioPageApi";
 export * from "./contentStudioModel";
