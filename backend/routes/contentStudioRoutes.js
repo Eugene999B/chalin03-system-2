3ece1ed6d850d3824391208d8c7e051af293c7e1
Eugene999B <kuamieugene321@gmail.com>
2026-08-10 05:53:38 +0000

    Mount link integrity beside public analytics on current CHALIN ONE

--- backend/routes/contentStudioRoutes.js
+++ backend/routes/contentStudioRoutes.js
@@ -11,6 +11,7 @@ const contentStudioAnalyticsRoutes = require("./contentStudioAnalyticsRoutes");
 const contentStudioCompanyInfoRoutes = require("./contentStudioCompanyInfoRoutes");
 const contentStudioCoreRoutes = require("./contentStudioCoreRoutes");
 const contentStudioFormRoutes = require("./contentStudioFormRoutes");
+const contentStudioLinkIntegrityRoutes = require("./contentStudioLinkIntegrityRoutes");
 const contentStudioMediaRoutes = require("./contentStudioMediaRoutes");
 const contentStudioNavigationRoutes = require("./contentStudioNavigationRoutes");
 const contentStudioRedirectRoutes = require("./contentStudioRedirectRoutes");
@@ -42,6 +43,9 @@ router.use("/forms", contentStudioFormRoutes);
 router.use("/newsroom", contentStudioNewsroomRoutes);
 router.use("/company-info", contentStudioCompanyInfoRoutes);
 router.use("/portfolio", contentStudioPortfolioRoutes);
+// Link integrity belongs to the existing Pages scope and is deliberately
+// mounted before the generic /pages/:pageId routes in contentStudioCoreRoutes.
+router.use("/pages/link-integrity", contentStudioLinkIntegrityRoutes);
 router.use("/", contentStudioCoreRoutes);
 
 router.use((error, req, res, next) => {
