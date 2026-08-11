66629e5acdfb800f779a9e0043bcbb72c1d2300b
Eugene999B <kuamieugene321@gmail.com>
2026-08-11 05:21:30 +0000

    Mount cashier-safe serialized sale scanner

--- backend/routes/inventoryTraceabilityRoutes.js
+++ backend/routes/inventoryTraceabilityRoutes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const inventoryTraceabilityCoreRoutes = require("./inventoryTraceabilityCoreRoutes");
 const inventoryTraceabilityReceivingRoutes = require("./inventoryTraceabilityReceivingRoutes");
 const inventoryLossDetectionRoutes = require("./inventoryLossDetectionRoutes");
+const inventorySaleScanRoutes = require("./inventorySaleScanRoutes");
 
 const router = express.Router();
 
 router.use("/receiving", inventoryTraceabilityReceivingRoutes);
 router.use("/loss-control", inventoryLossDetectionRoutes);
+router.use("/sale-scan", inventorySaleScanRoutes);
 router.use(inventoryTraceabilityCoreRoutes);
 
 module.exports = router;
