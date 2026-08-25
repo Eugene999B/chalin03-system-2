d8c9bc5a4ca3355a3c73d8af1f384f835328d298
Eugene999B <kuamieugene321@gmail.com>
2026-08-25 21:53:14 +0000

    feat: add optional customer profile photo storage

---
+++ database/migrations/20260825_equipment_finance_customer_profile_photo.sql
@@ -0,0 +1,9 @@
+ALTER TABLE hire_customers
+  ADD COLUMN profile_photo_data_url LONGTEXT NULL AFTER risk_notes;
+
+INSERT INTO schema_migrations (migration_name, description)
+VALUES (
+  '20260825_equipment_finance_customer_profile_photo',
+  'Adds an optional normalized portrait photo to the shared Hire/Finance customer master.'
+)
+ON DUPLICATE KEY UPDATE description = VALUES(description);
