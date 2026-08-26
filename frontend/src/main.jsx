26daf61d6974ada84ce4b08f2d1b489f43d4a25a
Eugene999B <kuamieugene321@gmail.com>
2026-08-26 16:23:03 +0000

    frontend: bump intelligence control room cache release to v51

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -19,9 +19,9 @@ import "./styles/loginHumanCopy.css";
 import "./styles/financeNumberLayout.css";
 
 const APP_BUILD_ID =
-  import.meta.env.VITE_CHALIN03_BUILD_ID || "browser-cache-integrity-v50-users-settings-intelligence";
-const APP_SHELL_RELEASE = `browser-cache-integrity-v50-${APP_BUILD_ID}`;
-const CACHE_RECOVERY_KEY = "__chalin03_frontend_recovery_v50__";
+  import.meta.env.VITE_CHALIN03_BUILD_ID || "browser-cache-integrity-v51-users-settings-intelligence";
+const APP_SHELL_RELEASE = `browser-cache-integrity-v51-${APP_BUILD_ID}`;
+const CACHE_RECOVERY_KEY = "__chalin03_frontend_recovery_v51__";
 
 installCommandGateHistoryTracker();
 installCriticalFinanceWorkspacePreload();
