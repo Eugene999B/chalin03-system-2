055788faa0ed7884b2cab8922cea5a64a0d0638c
Eugene999B <kuamieugene321@gmail.com>
2026-08-30 14:14:23 +0000

    style(login): restore original mobile login design

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -7,8 +7,6 @@ import "../styles/loginBusinessSelectionSync.css";
 import "../styles/loginArtworkScale.css";
 import "../styles/loginBusinessSelectionOriginalScale.css";
 import "../styles/loginEmojiRestore.css";
-import "../styles/loginDesktopParity.css";
-import "../styles/loginMobileDesktopVisualFinal.css";
 
 const TOKEN_KEY = "chalin03_token";
 const USER_KEY = "chalin03_user";
